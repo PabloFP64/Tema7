6577d2ca0cac061f1d25ab986b46cf548a3bf191
PabloFP64 <63953769+PabloFP64@users.noreply.github.com>
2023-03-14 16:38:14 +0100

    Terminado

---
+++ src/main/java/org/example/Cuadricula.java
@@ -0,0 +1,48 @@
+package org.example;
+
+public class Cuadricula {
+
+    final int T = 10;
+    int tabla[][] = new int [T][T];
+    String[][] interfaz = new String[T][T];
+
+
+
+    public Cuadricula() {
+        for (int j=0;j<T;j++){
+            for (int i=0;i<T;i++){
+                tabla[j][i]=0;
+            }
+        }
+
+        for (int j=0;j<T;j++){
+            for (int i=0;i<T;i++){
+                interfaz[j][i]="?";
+            }
+        }
+    }
+
+    public void depositarBarco(int x, int y){
+        tabla[x][y] = 1;
+    }
+
+    public boolean disparar(int x, int y, String player){
+        if(tabla[x][y] == 1){
+            System.out.println(String.format("%s hundido el barco",player));
+            return true;
+        }else{
+            System.out.println("Agua!");
+            interfaz[x][y] = "~";
+            return false;
+        }
+    }
+
+    public void visualizacion(){
+        for (int j=0;j<interfaz.length;j++){
+            for (int i=0;i< interfaz.length;i++){
+                System.out.print(" "+interfaz[j][i]);
+            }
+            System.out.println("");
+        }
+    }
+}
---
+++ src/main/java/org/example/Dado.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.Random;
+
+public class Dado {
+    public int numCaras;
+    public int ultCara;
+
+    public Dado(int numCaras) {
+        this.numCaras = numCaras;
+    }
+
+    public Dado(){
+        this.numCaras = 6;
+    }
+
+    public int getNumCaras() {
+        return this.numCaras;
+    }
+
+    public void setNumCaras(int numCaras) {
+        this.numCaras = numCaras;
+    }
+
+    public int getUltCara() {
+        return this.ultCara;
+    }
+
+    public int tirarDado(){
+        Random r = new Random();
+//        this.ultCara = r.nextInt(1, numCaras+1);
+        this.ultCara = (int) (Math.random() * (this.numCaras - 1)) + 1;
+        return this.ultCara;
+    }
+}
